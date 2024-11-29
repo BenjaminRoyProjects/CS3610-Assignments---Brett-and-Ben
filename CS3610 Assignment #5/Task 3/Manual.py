2123b99919031cb9dfa740da1678775f3a36bdc1
Benjamin Roy <83797502+BenjaminRoyProjects@users.noreply.github.com>
2024-11-29 15:01:10 -0700

    Update Manual.py

---
+++ CS3610 Assignment #5/Task 3/Manual.py
@@ -0,0 +1,17 @@
+class Manual:
+  "A concrete product class."
+  
+  def __init__(self) -> None:
+    self.__name = "Manual"
+    self.__components = []
+
+  def add(self, component):
+    print(f"Adding {component} to {self.__name}")
+    self.__components.append(component)
+    print(f"Current components: {self.__components}
+
+  def showComponents(self):
+    print(f"There are {len(self.__components)} components in {self.__name}.")
+    print("Components: ", end="")
+    for component in self.__components:
+      print(f"{component} ", end="")
