21f97d53d63816b65f923ad0e38d7692634a0c7b
Benjamin Roy <83797502+BenjaminRoyProjects@users.noreply.github.com>
2024-11-29 16:09:15 -0700

    commit

--- CS3610 Assignment #5/Task 2/Application.py
+++ CS3610 Assignment #5/Task 2/Application.py
@@ -1,25 +1,13 @@
-#from GUIFactory import GUIFactory
+from GUIFactory import GUIFactory
+from MacFactory import MacFactory
+from WinFactory import WinFactory
 from typing import Type
 
 class Application:
     "Client application."
 
-    "Static dictionary of supported operating systems and their corresponding factories."
-    supportedOSDict = {"WINDOWS": WinFactory, "MAC": MacFactory}
-
-    def __init__(self):
-        self.__factory = None
-
-        "Manually takes the OS as input. In a real implementation, would use the platform import to check it."
-        opsys = input("Enter OS: ").upper()
-
-        if(opsys not in self.supportedOSDict.keys()):
-            print("Operating system not supported. Supported operating systems:", list(self.supportedOSDict.keys()))
-            return
-        
-        "Creates a factory for the given OS, storing it in the factory attribute"
-        print(f"Creating a factory for {opsys} OS")
-        self.__factory = self.supportedOSDict[opsys]()
+    def __init__(self, factory: Type[GUIFactory]):
+        self.__factory = factory
 
     @property
     def factory(self) -> GUIFactory:
--- CS3610 Assignment #5/Task 2/client.py
+++ CS3610 Assignment #5/Task 2/client.py
@@ -1,4 +1,18 @@
 from Application import Application
+from MacFactory import MacFactory
+from WinFactory import WinFactory
 
-app1 = Application()
-print(app1.factory)
+"Static dictionary of supported operating systems and their corresponding factories."
+supportedOSDict = {"WINDOWS": WinFactory, "MAC": MacFactory}
+
+"Manually takes the OS as input. In a real implementation, would use the platform import to check it."
+while(True):
+    opsys = input("Enter OS: ").upper()
+    if(opsys not in self.supportedOSDict.keys()):
+        print("Operating system not supported. Supported operating systems:", list(self.supportedOSDict.keys()))
+    else:
+        break
+
+"Create the factory for the OS and make an Application using it"
+print(f"Creating a factory for {opsys} OS")
+app1 = Application(self.supportedOSDict[opsys]())
