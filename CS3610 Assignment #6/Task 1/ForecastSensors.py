9f774a92dd1ac859f6ba0813dd6c15d7566a9bf8
Benjamin Roy <83797502+BenjaminRoyProjects@users.noreply.github.com>
2024-12-06 10:20:25 -0700

    Update ForecastSensors.py

--- CS3610 Assignment #6/Task 1/ForecastSensors.py
+++ CS3610 Assignment #6/Task 1/ForecastSensors.py
@@ -6,17 +6,18 @@
 import random
 
 class ForecastSensors():
-    NUMREADINGS = 5 #Number of readings to "receive from the sensors"
-    TEMPDEVIATION = 5 #Deviation of "sensor data"
+    __NUMREADINGS = 5 #Number of readings to "receive from the sensors"
+    __TEMPDEVIATION = 5 #Deviation of "sensor data"
+    __RANDOMTEMPRANGE = 20 #Temperatures generated will be between the + and - of this integer
     
     def getForecastData(self):
         #Generate a list of temperatures "from the sensors". Returned as a binary list.
         avgTempList = []
-        randtemp = random.randrange(-40, 40) #Random temp that the others will be based on
-        for day in range(self.NUMREADINGS):
+        randtemp = random.randrange(0 - self.__RANDOMTEMPRANGE, self.__RANDOMTEMPRANGE) #Random temp that the others will be based on
+        for day in range(self.__NUMREADINGS):
             #Generate a temperature for each day based on the random temperature generated prior
-            avgTempList.append(random.randrange((randtemp - self.TEMPDEVIATION), (randtemp + self.TEMPDEVIATION)))
-        #Convert forecast data to binary
+            avgTempList.append(random.randrange((randtemp - self.__TEMPDEVIATION), (randtemp + self.__TEMPDEVIATION)))
+        #Convert forecast data to a list of bytes and return it
         binaryTemps = []
         for temp in avgTempList:
             binaryTemps.append(str(temp).encode("utf-8"))
