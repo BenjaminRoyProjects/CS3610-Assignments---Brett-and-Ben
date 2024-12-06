4d623092f9730bfdd94954d111023c294736c479
Benjamin Roy <83797502+BenjaminRoyProjects@users.noreply.github.com>
2024-12-06 10:21:17 -0700

    Update client.py

--- CS3610 Assignment #6/Task 1/client.py
+++ CS3610 Assignment #6/Task 1/client.py
@@ -6,16 +6,24 @@
 from ForecastSensors import ForecastSensors
 from ForecastSystem import ForecastSystem
 from Adapter import Adapter
-from ClientInterface import ClientInterface
-  
-#
-def forecast(self, service):
-  return service.getForecastData()
+from typing import Type
+
+class Client():
+  #Take a forecastsensor object and collect sensor data from it
+  def forecast(self, source: Type[ForecastSensors]):
+    print("Running forecast system...")
+    tempdata = source.getForecastData()
+    print(f"Sensor forecast data: {tempdata}", end="\n\n")
 
 #Main
+app1 = Client()
+sens1 = ForecastSensors()
+print("Using old system:")
+app1.forecast(sens1) #
 serv1 = ForecastSystem()
-print(forecast(serv1))
-
+adap1 = Adapter(serv1)
+print("Using new system:")
+app1.forecast(adap1)
 
 #cd CS3610\ Assignment\ #6/Task\ 1
 #python client.py
