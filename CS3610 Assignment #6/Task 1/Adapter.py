1f50eeefc3cecac0a05cd4c01b6e6be46acf86bd
Benjamin Roy <83797502+BenjaminRoyProjects@users.noreply.github.com>
2024-12-06 10:43:25 -0700

    Update Adapter.py

--- CS3610 Assignment #6/Task 1/Adapter.py
+++ CS3610 Assignment #6/Task 1/Adapter.py
@@ -23,14 +23,15 @@ class Adapter(ForecastSensors):
         for tempreading in strData:
             tmp = ElTree.SubElement(tempdata, f"temp")
             tmp.text = tempreading
-        return tempdata
+        #Convert the XML tree into an XML string
+        strXMLData = ElTree.tostring(tempdata, encoding="unicode")
+        return strXMLData
         
     #Get data from the forecast sensors, convert it, and pass it to the forecast system
     def getForecastData(self):
         #Use ForecastSensors method to fetch data
         binData = super().getForecastData()
-        #Convert the data from binary to an XML ElementTree to a string of XML data
+        #Convert the data from binary to an XML string
         xmlData = self.binToXML(binData)
-        strXMLData = ElTree.tostring(xmlData, encoding="unicode")
         #Pass the xml data to the new system and return its cleaner data
-        return self.__fcsystem.cleanXMLData(strXMLData)
+        return self.__fcsystem.cleanXMLData(xmlData)
