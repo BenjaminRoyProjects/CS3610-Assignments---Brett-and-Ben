0ac55276bbaacf5d762d86cd4ace82c66ada709e
Benjamin Roy <83797502+BenjaminRoyProjects@users.noreply.github.com>
2024-12-06 15:36:58 -0700

    Update Director.py

--- CS3610 Assignment #5/Task 3/Director.py
+++ CS3610 Assignment #5/Task 3/Director.py
@@ -1,21 +1,35 @@
-from Typing import Type
+#Director
+#CS3610 Assignment #5
+#Brett and Ben
+#Dec 6, 2024
+
+from typing import Type
+from Builder import Builder
 
 class Director:
     "Class usable by client code to automatically control builders."
 
-    def __init__(self):
-        pass
+    def __init__(self, builder: Type[Builder]):
+        self.__builder = builder
 
-    def makeSUV(self, builder):
-        builder.reset()
-        builder.setSeats("4")
-        builder.setEngine("SUVEngine")
-        builder.setTripComputer()
-        builder.setGPS()
+    @property
+    def builder(self) -> Builder:
+        return self.__builder
 
-    def makeSportsCar(self, builder):
-        builder.reset()
-        builder.setSeats("2")
-        builder.setEngine("SportEngine")
-        builder.setTripComputer()
-        builder.setGPS()
+    @builder.setter
+    def builder(self, newbuilder: Type[Builder]) -> None:
+        self.__builder = newbuilder
+
+    def makeSUV(self) -> None:
+        self.__builder.reset()
+        self.__builder.setSeats("4")
+        self.__builder.setEngine("SUVEngine")
+        self.__builder.setTripComputer()
+        self.__builder.setGPS()
+
+    def makeSportsCar(self) -> None:
+        self.__builder.reset()
+        self.__builder.setSeats("2")
+        self.__builder.setEngine("SportEngine")
+        self.__builder.setTripComputer()
+        self.__builder.setGPS()
