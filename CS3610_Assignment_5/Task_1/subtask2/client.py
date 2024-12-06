c44a4e3077dc02228934a1e7efeb492cd4be7838
liint <48611109+liint@users.noreply.github.com>
2024-12-06 16:09:43 -0700

    commit

--- CS3610_Assignment_5/Task_1/subtask2/client.py
+++ CS3610_Assignment_5/Task_1/subtask2/client.py
@@ -1,7 +1,17 @@
 from documentCreator import documentCreator
+from pdfDocumentCreator import pdfDocumentCreator
+from wordDocumentCreator import wordDoucmentCreator
+from txtDocumentCreator import txtDocumentCreator
 
 testDocs = ['pdf','txt',"word",'word','txt']
+
 for name in testDocs:
-    res = documentCreator.create_document(name)
+    if name.lower() == 'pdf':
+        creator = pdfDocumentCreator()
+    elif name.lower() == 'word':
+        creator = wordDoucmentCreator()
+    elif name.lower() == 'txt':
+        creator = txtDocumentCreator()
+    res = creator.factory_method()
     if res:
         print(res.objType)
