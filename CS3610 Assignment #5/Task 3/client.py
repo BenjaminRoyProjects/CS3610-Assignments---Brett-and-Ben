b87b688ae2252c3be0d3c15972cd8d331efed8b3
Benjamin Roy <83797502+BenjaminRoyProjects@users.noreply.github.com>
2024-12-06 15:36:36 -0700

    Update client.py

--- CS3610 Assignment #5/Task 3/client.py
+++ CS3610 Assignment #5/Task 3/client.py
@@ -1,11 +1,26 @@
-from director import Director
-from builder import Builder
-from carBuilder import CarBuilder
-from manualBuilder import ManualBuilder
+#client
+#CS3610 Assignment #5
+#Brett and Ben
+#Dec 6, 2024
 
-director = Director()
-builder = CarBuilder()
-mBuilder = ManualBuilder()
-#director.makeSUV(builder)
-director.makeSUV(mBuilder)
-manual = builder.getResult()
+from Director import Director
+from Builder import Builder
+from CarBuilder import CarBuilder
+from CarManualBuilder import ManualBuilder
+
+builder1 = CarBuilder()
+director1 = Director(builder1) #Car -uilding director
+director1.makeSportsCar()
+car1 = director1.builder.getResult()
+car1.showComponents()
+
+print("")
+
+builder2 = ManualBuilder()
+director1.builder = builder2 #Manual-building director
+director1.makeSUV()
+manual1 = director1.builder.getResult()
+manual1.showComponents()
+
+#cd CS3610\ Assignment\ #5/\Task\ 3
+#python client.py
