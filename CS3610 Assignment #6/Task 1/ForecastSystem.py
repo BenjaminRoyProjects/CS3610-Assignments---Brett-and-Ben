0477836782002ad0a78f241deac1ea9ef276adb7
Benjamin Roy <83797502+BenjaminRoyProjects@users.noreply.github.com>
2024-12-06 10:19:01 -0700

    Update ForecastSystem.py

--- CS3610 Assignment #6/Task 1/ForecastSystem.py
+++ CS3610 Assignment #6/Task 1/ForecastSystem.py
@@ -7,7 +7,24 @@ from typing import Type
 import xml.etree.ElementTree as ElTree
 
 class ForecastSystem():
-  def cleanXMLData(self, data: Type[xml]):
-    tempdata = ElTree.fromstring(data)
-    print(tempdata)
-    
+  def cleanXMLData(self, data: Type[str]):
+    #Display input data
+    print("Forecast System has received input data:")
+    print(data)
+    #Convert the xml string back into an element tree
+    xmltree = ElTree.fromstring(data)
+    #Extract the reading data from the element tree
+    readings = xmltree.findall(".//temp")
+    #Find the average of the temperatures
+    average = 0
+    for reading in readings:
+      average += int(reading.text)
+    average = average / len(readings)
+    #Form a string of temperature readings averaged with the overall average of the readings reading
+    listtemps = []
+    for reading in readings:
+      #Average the reading with the overall average, rounded to one decimal place
+      accreading = (int(reading.text) + average)/2
+      accreading = round(accreading, 1)
+      listtemps.append(accreading)
+    return listtemps
