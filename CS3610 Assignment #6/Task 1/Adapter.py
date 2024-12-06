738d4b5cf9cee3b63a9639351ffd8f4daafff8de
Benjamin Roy <83797502+BenjaminRoyProjects@users.noreply.github.com>
2024-12-06 10:19:34 -0700

    Update Adapter.py

--- CS3610 Assignment #6/Task 1/Adapter.py
+++ CS3610 Assignment #6/Task 1/Adapter.py
@@ -7,36 +7,30 @@ from ForecastSensors import ForecastSensors
 from ForecastSystem import ForecastSystem
 from typing import Type
 import xml.etree.ElementTree as ElTree
- 
+
 class Adapter(ForecastSensors):
-    def __init__(self, service: Type[ForecastSystem]): 
-        self.__adaptee = service #Instance of ForecastSystem class
+    def __init__(self, forecastservice: Type[ForecastSystem]): 
+        self.__fcsystem = forecastservice #Instance of ForecastSystem class
 
     #Convert binary temperature data to xml format
-    def binToXML(self, data: Type[bytes])
+    def binToXML(self, data: Type[bytes]):
         strData = []
         for entry in data:
             strData.append(entry.decode("utf-8"))
         #Convert the list of strings into an XML tree
         tempdata = ElTree.Element("tempdata")
-        tempdatadict = {}
-        i = 0
+        tmp = None
         for tempreading in strData:
-            tempdatadict[i] = ElTree.SubElement(tempdata, f"Reading {i}")
-            tempdatadict[i].text = tempreading
-            i += 1
-        return ElTree.tostring(tempdata, "unicode")
+            tmp = ElTree.SubElement(tempdata, f"temp")
+            tmp.text = tempreading
+        return tempdata
         
     #Get data from the forecast sensors, convert it, and pass it to the forecast system
     def getForecastData(self):
         #Use ForecastSensors method to fetch data
         binData = super().getForecastData()
+        #Convert the data from binary to an XML ElementTree to a string of XML data
         xmlData = self.binToXML(binData)
-        #Pass the XML data to the ForecastSystem
-        self.__adaptee.cleanXMLData(xmlData)
-        
-        
-        #Return the XML formatted data
-        return 
-        
-    
+        strXMLData = ElTree.tostring(xmlData, encoding="unicode")
+        #Pass the xml data to the new system and return its cleaner data
+        return self.__fcsystem.cleanXMLData(strXMLData)
