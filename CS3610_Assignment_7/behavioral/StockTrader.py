df340b84c72999e68751a2ddf27b4fc2f37667f0
Benjamin Roy <83797502+BenjaminRoyProjects@users.noreply.github.com>
2024-12-10 09:12:20 -0700

    commit

--- CS3610_Assignment_7/behavioral/StockTrader.py
+++ CS3610_Assignment_7/behavioral/StockTrader.py
@@ -3,5 +3,25 @@
 #Brett and Ben
 #StockTrader
 
+from typing import Type
+#from StockSubscriber import StockSubscriber
+
 class StockTrader:
+    def __init__():
+        self.__subscribers = set() #Implemented as a set to prevent duplicate subscriptions
+
+    def addSubscriber(subscriber: Type[SubscriberInt]):
+        print(f"Removing subscriber {subscriber}...", end="")
+        self.__subscribers.add(subscriber)
+
+    def removeSubscriber(subscriber: Type[SubscriberInt]):
+        self.__subscribers.remove(subscriber)
+
+class SubscriberInt():
     pass
+class StockSubscriber(SubscriberInt):
+    pass
+
+trader1 = StockTrader()
+sub1 = StockSubscriber()
+trader1.addSubscriber(sub1)
