9ffa3e59b80076279cdfef2447493303de627830
Benjamin Roy <83797502+BenjaminRoyProjects@users.noreply.github.com>
2024-12-15 15:08:43 -0700

    commit

--- CS3610_Assignment_7/behavioral/SubscriberInt.py
+++ CS3610_Assignment_7/behavioral/SubscriberInt.py
@@ -9,7 +9,7 @@ from typing import Dict, Type
 #Subscriber interface
 class SubscriberInt(ABC):
     
-    #Update the subscriber's copy of the stock value data
+    #Update the subscriber's copy of the stock value data.
     #Parameters:
     #   stockvalues: {String, Float}. A copy of the trader's stock data dictionary, to be used to update the subscriber's personal dictionary.
     #Returns: Nothing.
@@ -17,7 +17,7 @@ class SubscriberInt(ABC):
     def update(self, stockvalues: Dict[str, float]):
         pass
 
-    #Remove an entry from the subscriber's stock data dictionary
+    #Remove an entry from the subscriber's stock data dictionary.
     #Parameters:
     #   stockID: String. The name of the stock to be removed as it appears in the subscriber's dictionary.
     #Returns: Nothing.
